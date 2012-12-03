7ee33c7fb8772d337cc6d627b6212425daea14a8
Pavel Guscha <pavimus@gmail.com>
2012-12-04 00:36:07 +0300

    database settings for staging server

--- protected/config/server_staging.php
+++ protected/config/server_staging.php
@@ -4,7 +4,7 @@ $config = CMap::mergeArray(
         require((YII_DEBUG ? 'web_debug.php' : 'web_production.php')), array(
             'components' => array(
                 'db' => array(
-                    'connectionString' => 'mysql:host=localhost;dbname=nomtel',
+                    'connectionString' => 'mysql:host=localhost;dbname=www_nomtel',
                     'username' => 'www_nomtel',
                     'password' => '9Z4b764fY0WzglOocmeB',
                 ),
