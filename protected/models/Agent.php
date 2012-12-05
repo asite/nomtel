02ca4d96a79479e42c01c169bbb876a484e1e1c6
Pavel Guscha <pavimus@gmail.com>
2012-12-06 00:03:10 +0300

    use 'surname name' as agent representation

--- protected/models/Agent.php
+++ protected/models/Agent.php
@@ -8,39 +8,7 @@ class Agent extends BaseAgent
 		return parent::model($className);
 	}
 
-/*
-    public function convertDateTimeFieldsToEDateTime() {
-        // rest of work will do setAttribute() routine
-        $this->setAttribute('birthday_date',strval($this->birthday_date));
-        $this->setAttribute('passport_issue_date',strval($this->birthday_date));
+    public function __toString() {
+        return $this->surname.' '.$this->name;
     }
-
-    public function convertDateTimeFieldsToString() {
-        if (is_object($this->birthday_date) && get_class($this->birthday_date)=='EDateTime') $this->birthday_date=new EString($this->birthday_date->format(self::$mySqlDateFormat));
-        if (is_object($this->passport_issue_date) && get_class($this->passport_issue_date)=='EDateTime') $this->passport_issue_date=new EString($this->passport_issue_date->format(self::$mySqlDateFormat));
-    }
-
-    public function afterFind() {
-        $this->convertDateTimeFieldsToEDateTime();
-    }
-
-    public function setAttribute($column,$val) {
-        if (is_string($val)) {
-            if ($column=='birthday_date') $val=$val ? new EDateTime($val,null,'date'):null;
-            if ($column=='passport_issue_date') $val=$val ? new EDateTime($val,null,'date'):null;
-        }
-        parent::setAttribute($column,$val);
-    }
-
-    public function beforeSave()
-    {
-        $this->convertDateTimeFieldsToString();
-
-        return true;
-    }
-
-    public function afterSave() {
-        $this->convertDateTimeFieldsToEDateTime();
-    }
-*/
 }
