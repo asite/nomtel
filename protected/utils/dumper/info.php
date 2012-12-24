df3f07f1615723f5e8bf9fbf495abd73854e5c14
gghz <gghz@bk.ru>
2012-12-24 09:06:49 +0600

    Supex dumper pro

--- protected/utils/dumper/info.php
+++ protected/utils/dumper/info.php
@@ -1,23 +1,21 @@
 <?php
-require_once('kiss.php');
-
 // Sypex Dumper Lite 2
 header("Content-Type: text/html; charset=utf-8");
 set_error_handler("sxd_error");
 if(!empty($_POST['ajax']['job']) && preg_match("/^[\w-]+$/", $_POST['ajax']['job'])){
 	$d = date("'Y.m.d H:i:s'");
 	if(!empty($_COOKIE['sxd'])) {
-		include(SES_FILE);
+		include('ses.php');
 		if(!empty($SES[$_COOKIE['sxd']])) {
 			$CFG = &$SES[$_COOKIE['sxd']]['cfg'];
 			include(load_lang($SES[$_COOKIE['sxd']]['lng']));
 		}
 	}
 	if(empty($LNG)) {
-		include(CFG_FILE);
+		include('cfg.php');
 		include(load_lang($CFG['lang']));
 		if(!empty($CFG['auth'])) {echo "sxd.log.add({$d},[" . esc($LNG['stop_5']) . " (1)]);sxd.hideLoading();"; exit;}
-	}
+	} 
 	$job_name = $_POST['ajax']['job'];
 	$log_seek = !empty($_POST['ajax']['lseek']) ? (int) $_POST['ajax']['lseek'] : 0;
 	$job_file = "{$CFG['backup_path']}{$job_name}.job.php";
@@ -47,7 +45,7 @@ if(!empty($_POST['ajax']['job']) && preg_match("/^[\w-]+$/", $_POST['ajax']['job
 						$old_time = $t[0];
 						$logs = array();
 					}
-					$logs[] = esc($t[2]);
+					$logs[] = esc($t[2]);	
 				}
 				if(!empty($logs)) $log .= "sxd.log.add('{$old_time}',[" . implode(',', $logs) . "]);";
 			}
@@ -55,11 +53,11 @@ if(!empty($_POST['ajax']['job']) && preg_match("/^[\w-]+$/", $_POST['ajax']['job
 			echo $log . "sxd.job.log_seek = {$log_seek};";
 			// Читаем лог
 			if($f[4] == 'EOJ') {
-				$pt = $pc = 100;
+				$pt = $pc = 100; 
 				fclose($lh);
 				fclose($fh);
-				if (function_exists('usleep')) usleep(400000);
-				else sleep(1);
+				/*if (function_exists('usleep')) usleep(400000);
+				else sleep(1);*/
 				if($JOB['act'] == 'backup') $f[3] = filesize(file_exists($JOB['file_name']) ? $JOB['file_name'] : $JOB['file_tmp']);
 				// Обновляем список файлов
 				if($JOB['act'] == 'backup') print "sxd.actions.filelist(); z('btn_down').file = '{$JOB['file']}'; z('btn_down').style.display = '';";
@@ -74,28 +72,28 @@ if(!empty($_POST['ajax']['job']) && preg_match("/^[\w-]+$/", $_POST['ajax']['job
 			}
 			else{
 				if($JOB['act'] == 'backup') $f[3] = filesize($JOB['file_tmp']);
-				if($f[4] != 'EK' && time() > $f[1] + 30) {
+				if($f[4] != 'EK' && time() > $f[1] + 10) {
 					fopen($JOB['file_stp'],'w');
 					$f[9] = 0;
 					$f[1] = $time;
 					fwrite($fh, implode("\t", $f) . "\n");
 					echo "sxd.log.add({$d},[" . esc($LNG['job_freeze']) . "]);sxd.hideLoading();z('btn_resume').style.display = '';z('btn_pause').style.display = 'none';";
 				}
-				echo "sxd.timer.set({$f[0]},{$time},{$pt});sxd.progress.current.set({$pc},0,{$f[7]}, {$f[8]});sxd.progress.total.set({$pt},{$f[3]});";
+				echo "sxd.timer.set({$f[0]},{$time},{$pt});sxd.progress.current.set({$pc},0,{$f[7]}, {$f[8]});sxd.progress.total.set({$pt},{$f[3]});";	
 			}
 		break;
-		case 'stop':
+		case 'stop': 
 			$fs = fopen($JOB['file_stp'],'w');
 			fwrite($fs, 1);
 			echo "sxd.log.add({$d},[" . esc($LNG['stop_job']) . "]);";
 		break;
-		case 'pause':
+		case 'pause': 
 			fopen($JOB['file_stp'],'w');
 			echo "sxd.log.add({$d},[" . esc($LNG['stop_job']) . "]);";
 		break;
 	}
 }
-else echo "sxd.hideLoading();";
+else echo "sxd.hideLoading();";	
 
 function load_lang($lng_name = 'auto'){
 	if($lng_name == 'auto'){
@@ -121,10 +119,10 @@ function sxd_error($errno, $errmsg, $filename, $linenum, $vars){
 	global $JOB;
 	if(strpos($errmsg, 'timezone settings')) return;
 	if(!empty($JOB['file_stp'])) fopen($JOB['file_stp'],'w');
-	$errortype = array(1 => 'Error', 2 => 'Warning', 4 => 'Parsing Error', 8 => 'Notice', 16 => 'Core Error', 32 => 'Core Warning', 64 => 'Compile Error',
+	$errortype = array(1 => 'Error', 2 => 'Warning', 4 => 'Parsing Error', 8 => 'Notice', 16 => 'Core Error', 32 => 'Core Warning', 64 => 'Compile Error', 
 					   128 => 'Compile Warning', 256 => 'User Error', 512 => 'User Warning', 1024 => 'User Notice');
 	$str = "{$errortype[$errno]}: {$errmsg} ({$filename}:{$linenum})";
-	//error_log("[info.php]\n{$str}\n", 3, "error.log");
+	//error_log("[info.php]\n{$str}\n", 3, "error.log"); 
     if($errno == 8 || $errno == 1024) {
     	echo "sxd.log.add('" . date("Y.m.d H:i:s") . "',['" . mysql_escape_string($str) . "'], 4);sxd.hideLoading();";;
 	}
@@ -132,6 +130,6 @@ function sxd_error($errno, $errmsg, $filename, $linenum, $vars){
     	echo "sxd.log.add('" . date("Y.m.d H:i:s") . "',['" . mysql_escape_string($str) . "'], 4);sxd.hideLoading();";
     	die;
 	}
-}
+} 
 
 ?>
