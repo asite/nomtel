9c815faa658cdf1a2ad26a5d0f115f9d53428dd2
Pavel Guscha <pavimus@gmail.com>
2012-12-24 14:51:15 +0300

    fixed filtering delivery reports by id

--- protected/models/DeliveryReport.php
+++ protected/models/DeliveryReport.php
@@ -11,4 +11,23 @@ class DeliveryReport extends BaseDeliveryReport
     public function __toString() {
         return '№'.$this->id.' '.Yii::t('app','from').' '.$this->dt;
     }
+
+    public function search() {
+        $criteria = new CDbCriteria;
+
+        $criteria->alias='delivery_report';
+        $criteria->compare('delivery_report.id', $this->id);
+        $criteria->compare('delivery_report.agent_id', $this->agent_id);
+        $criteria->compare('delivery_report.bonus_report_id', $this->bonus_report_id);
+        $criteria->compare('delivery_report.dt', $this->dt, true);
+        $criteria->compare('delivery_report.sum', $this->sum);
+
+        $dataProvider=new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+
+        $dataProvider->pagination->pageSize=self::ITEMS_PER_PAGE;
+        return $dataProvider;
+    }
+
 }
