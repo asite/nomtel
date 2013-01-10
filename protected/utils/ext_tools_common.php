7df979468cc18bab4f36660cfd30fb6d8fc4cb92
Pavel Guscha <pavimus@gmail.com>
2013-01-10 15:44:25 +0300

    small authorization fix

--- protected/utils/ext_tools_common.php
+++ protected/utils/ext_tools_common.php
@@ -3,4 +3,5 @@
 if ($_SERVER['PHP_AUTH_USER']!='additional' && $_SERVER['PHP_AUTH_PW']!='protection') {
     header('WWW-Authenticate: Basic realm="Please enter your credentials"');
     header('HTTP/1.0 401 Unauthorized');
+    exit;
 }
