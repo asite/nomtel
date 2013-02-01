0595d61f3d049b8a9cd41e5ca1b666cb09e47e10
nomtel <nomtel@www.asiteplace.ru>
2013-02-01 09:06:42 +0400

    config update

--- protected/config/server_web_asite_development.php
+++ protected/config/server_web_asite_development.php
@@ -4,14 +4,16 @@ $config = CMap::mergeArray(
     require((YII_DEBUG ? 'web_debug.php' : 'web_production.php')), array(
         'components' => array(
             'db' => array(
-                'connectionString' => 'mysql:host=localhost;dbname=crm_nomtel',
-                'username' => 'root',
-                'password' => '',
+                'connectionString' => 'mysql:host=localhost;dbname=crm',
+                'username' => 'crm',
+                'password' => '30i5FQoR',
             ),
         ),
         'params' => array(
             'adminEmail' => 'gghz@bk.ru',
-            'adminEmailFrom' => 'info@nomtel.com'
+            'adminEmailFrom' => 'info@asiteapp.ru',
+            'supportEmail' => array('alegudmail@gmail.com', '9292000001@mail.ru'),
+            'supportEmailFrom' => 'info@asiteapp.ru'
         )
     )
 );
