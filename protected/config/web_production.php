8516d6ea14f6a269d3dbc9e04905ad6683921312
nomtel <nomtel@www.asiteplace.ru>
2013-02-01 09:09:55 +0400

    link assets

--- protected/config/web_production.php
+++ protected/config/web_production.php
@@ -55,7 +55,7 @@ return array(
             ),
         ),
         'assetManager' => array(
-            'linkAssets' => true,
+            //'linkAssets' => true,
         ),
         'clientScript' => array(),
         'db' => array(
