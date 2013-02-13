8216a33542c4c3e540b8b41f2a5a04b75c3c01b6
Pavel Guscha <pavimus@gmail.com>
2013-02-13 16:52:14 +0300

    send sms only in production mode, in debug only write it to log file

--- protected/components/Sms.php
+++ protected/components/Sms.php
@@ -3,6 +3,11 @@
 class Sms
 {
     public static function send($number,$message) {
+        if (YII_DEBUG) {
+            Yii::log("SMS to number $number: $message");
+            return;
+        }
+
         $number=urlencode($number);
         $message=urlencode($message);
         file_get_contents("http://api.infosmska.ru/interfaces/SendMessages.ashx?login=ghz&pwd=zerozz&phones=7$number&sender=nomtel&message=$message");
