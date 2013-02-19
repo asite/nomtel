30184e7a4f4464f7bdeeccb6613259ab99b5d95a
Pavel Guscha <pavimus@gmail.com>
2013-02-20 00:02:18 +0300

    work on email importer

--- protected/components/MegafonBalanceEmailImporter.php
+++ protected/components/MegafonBalanceEmailImporter.php
@@ -1,6 +1,8 @@
 <?php
 class MegafonBalanceEmailImporter
 {
+    const STATUS_ANALYZE_PERIODS=14;
+
     private $emailProcessor;
 
     public function __construct() {
@@ -13,6 +15,43 @@ class MegafonBalanceEmailImporter
 
     private function log($msg,$level=CLogger::LEVEL_INFO) {
         Yii::log($msg,$level,__CLASS__);
+    }
+
+    private function recalcNumberBalanceStatus($number) {
+        $data=Yii::app()->db->createCommand("
+            select brn.balance
+            left outer join balance_report_number brn on (brn.number_id=:number_id and brn.balance_report_id=br.id)
+            from balance_report br
+            order by br.dt desc
+            limit 0,".self::STATUS_ANALYZE_PERIODS."
+        ")->queryAll(array(':number_id'=>$number->id));
+
+        $allPositive=true;
+        $allNegative=true;
+        $allStatic=true;
+
+        for($i=count($data)-1;$i>=0;$i--) {
+            $curVal=floatval($data[$i]);
+
+            if ($curVal>=0) $allNegative=false; else $allPositive=false;
+            if ($i>0 && abs($curVal-$data[$i-1])>1e-4) $allStatic=false;
+        }
+
+        $newBalanceStatus=Number::BALANCE_STATUS_NORMAL;
+
+        if ($allStatic) {
+            $newBalanceStatus=$allPositive ? Number::BALANCE_STATUS_POSITIVE_STATIC:Number::BALANCE_STATUS_NEGATIVE_STATIC;
+        } else {
+            if ($allPositive) $newBalanceStatus=Number::BALANCE_STATUS_POSITIVE_DYNAMIC;
+            if ($allNegative) $newBalanceStatus=Number::BALANCE_STATUS_NEGATIVE_DYNAMIC;
+        }
+
+        if (count($data)==1) $newBalanceStatus=Number::BALANCE_STATUS_NEW;
+
+        if ($newBalanceStatus!=$number->balance_status) {
+            $number->balance_status=$newBalanceStatus;
+            $number->save();
+        }
     }
 
     private function process($data) {
@@ -42,9 +81,18 @@ class MegafonBalanceEmailImporter
 
         $balanceReportNumber=BalanceReportNumber::model()->findByAttributes(array(
             'number_id'=>$number->id,
-            'balance_report_id'=>$balanceReport->id;
-        ))
+            'balance_report_id'=>$balanceReport->id
+        ));
 
+        if (!$balanceReportNumber) {
+            $balanceReportNumber=new BalanceReportNumber();
+            $balanceReportNumber->balance_report_id=$balanceReport->id;
+            $balanceReportNumber->number_id=$number->id;
+        }
+
+        $balanceReport->save();
+
+        $this->recalcNumberBalanceStatus($number);
 
         $trx->commit();
 
