41c449c0f8150b06ab26cc457caf8d5c2e62370a
nomtel <nomtel@www.asiteplace.ru>
2013-03-06 10:05:58 +0400

    nepomny

--- protected/views/layout/main.php
+++ protected/views/layout/main.php
@@ -265,5 +265,41 @@ array(
 </div>
 </div>
 </div>
+<?  if (Yii::app()->user->role == 'number') {?>
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = '42083';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter20314267 = new Ya.Metrika({id:20314267,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/20314267" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<?}?>
 </body>
 </html>
--- protected/views/pOSite/login.php
+++ protected/views/pOSite/login.php
@@ -8,7 +8,7 @@
 ?>
     <div class="modal pOLogin" style="margin-top: -230px;">
         <div class="modal-header cfix">
-            <img src="/static/logo.png" alt="" align="right"/><h3 style="line-height: 54px;">Добро пожаловать!</h3>
+            <a href="http://nomtel.ru"><img src="/static/logo.png" alt="" align="right"/></a><h3 style="line-height: 54px;">Добро пожаловать!</h3>
         </div>
         <div class="modal-body">
               В систему самообслуживания Вашего номера. Здесь Вы можете:
@@ -49,5 +49,38 @@
             <?php $this->widget('bootstrap.widgets.TbButton',array('label'=>'Восстановить пароль','buttonType'=>'submit','htmlOptions'=>array('name'=>'restore'))); ?>
         </div>
     </div>
+    <!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = '42083';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter20314267 = new Ya.Metrika({id:20314267,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/20314267" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 <?php $this->endWidget(); ?>
