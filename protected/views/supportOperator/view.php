75c511470c46c5659417b4d1688addd1ca6786b0
Pavel Gushcha <pavimus@gmail.com>
2013-03-07 00:34:35 +0300

    add operator stats to view page

--- protected/views/supportOperator/view.php
+++ protected/views/supportOperator/view.php
@@ -5,6 +5,43 @@ $this->breadcrumbs = array(
 );
 ?>
 
+<h1><?=Yii::t('app','Statistic')?></h1>
+<h3>По номерам</h3>
+<table class="table">
+    <thead>
+    <tr>
+        <th style="width:300px;"><?php echo Yii::t('app','Action') ?></th>
+        <th><?php echo Yii::t('app','Number of completed') ?></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($data as $k=>$v) { ?>
+    <tr>
+        <td><?php echo Number::getSupportStatusLabel($k) ?></td>
+        <td><?php echo $v ?></td>
+    </tr>
+        <?php }; ?>
+    </tbody>
+</table>
+
+<h3>По обращениям</h3>
+<table class="table">
+    <thead>
+    <tr>
+        <th style="width:300px;">Тип</th>
+        <th>Количество</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($ticketsStats as $k=>$v) { ?>
+    <tr>
+        <td><?=CHtml::encode($k)?></td>
+        <td><?=CHtml::encode($v)?></td>
+    </tr>
+        <?php }; ?>
+    </tbody>
+</table>
+
 <h1><?=Yii::t('app','Operator Numbers')?></h1>
 
 <?php $form = $this->beginWidget('BaseTbActiveForm', array(
