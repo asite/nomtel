c8735ddb4010be4f5679281a2ad3d4fd12a21c72
Pavel Guscha <pavimus@gmail.com>
2013-04-01 17:50:05 +0300

    fix filtering

--- static/script.js
+++ static/script.js
@@ -3,7 +3,6 @@ function createFiltersForm(id,token) {
     $('#filtersForm').append('<input type="hidden" name="exportGridView" value="'+id+'">');
     $('#filtersForm').append('<input type="hidden" name="YII_CSRF_TOKEN" value="'+token+'">');
     $('#'+id).find('select, input[type=text]').each(function(){
-        alert($(this).val());
         $('#filtersForm').append('<input type="hidden" name="'+$(this).attr('name')+'" value="'+$(this).val()+'">');
     })
     $('#filtersForm').submit();
