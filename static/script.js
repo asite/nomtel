e3f8e8aa8fa0b700d26803daf3c6cc25cf1aa0b0
Pavel Guscha <pavimus@gmail.com>
2013-04-03 13:31:28 +0300

    fix bug create csv + button free in muss change

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ function createFiltersForm(id,token) {
     $('body').append('<form id="filtersForm" method="post"></form>');
     $('#filtersForm').append('<input type="hidden" name="exportGridView" value="'+id+'">');
     $('#filtersForm').append('<input type="hidden" name="YII_CSRF_TOKEN" value="'+token+'">');
-    $('#'+id).find('select, input[type=text]').each(function(){
+    $('#'+id).find('select').each(function(){
         $('#filtersForm').append('<input type="hidden" name="'+$(this).attr('name')+'" value="'+$(this).val()+'">');
     })
     $('#filtersForm').submit();
