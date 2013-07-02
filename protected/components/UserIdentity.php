88ba08fecc9e11be9bea03ee3dcff833d2413f4b
Pavel Gushcha <pavimus@gmail.com>
2013-07-02 23:46:41 +0300

    change role fixes

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -18,15 +18,6 @@ class UserIdentity extends CUserIdentity
                     return true;
                 }
             } else {
-                $agent = Agent::model()->findByAttributes(array('user_id' => $this->_id));
-                if ($agent) {
-                    $this->setState('agentId', $agent->id);
-                    $this->setState('role',$agent->id==adminAgentId() ? 'admin':'agent');
-                    $this->setState('username',$agent->user->username);
-                    if ($agent->id==adminAgentId())
-                        $this->setState('supportOperatorId',SupportOperator::OPERATOR_ADMIN_ID);
-                    return true;
-                }
                 $supportOperator = SupportOperator::model()->findByAttributes(array('user_id' => $this->_id));
                 if ($supportOperator) {
                     $this->setState('manyRolesAvailable',$supportOperator->role=='supportSuper');
@@ -34,6 +25,15 @@ class UserIdentity extends CUserIdentity
                     $this->setState('agentId', adminAgentId());
                     $this->setState('role',$supportOperator->role);
                     $this->setState('username',$supportOperator->user->username);
+                    return true;
+                }
+                $agent = Agent::model()->findByAttributes(array('user_id' => $this->_id));
+                if ($agent) {
+                    $this->setState('agentId', $agent->id);
+                    $this->setState('role',$agent->id==adminAgentId() ? 'admin':'agent');
+                    $this->setState('username',$agent->user->username);
+                    if ($agent->id==adminAgentId())
+                        $this->setState('supportOperatorId',SupportOperator::OPERATOR_ADMIN_ID);
                     return true;
                 }
             }
