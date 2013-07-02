8090aeb986613e9c5560603974ef9df88f04286d
Pavel Gushcha <pavimus@gmail.com>
2013-07-02 23:40:12 +0300

    change role fixes

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -18,19 +18,19 @@ class UserIdentity extends CUserIdentity
                     return true;
                 }
             } else {
+                $supportOperator = SupportOperator::model()->findByAttributes(array('user_id' => $this->_id));
+                if ($supportOperator) {
+                    $this->setState('supportOperatorId', $supportOperator->id);
+                    $this->setState('agentId', adminAgentId());
+                    $this->setState('role',$supportOperator->role);
+                    return true;
+                }
                 $agent = Agent::model()->findByAttributes(array('user_id' => $this->_id));
                 if ($agent) {
                     $this->setState('agentId', $agent->id);
                     $this->setState('role',$agent->id==adminAgentId() ? 'admin':'agent');
                     if ($agent->id==adminAgentId())
                         $this->setState('supportOperatorId',SupportOperator::OPERATOR_ADMIN_ID);
-                    return true;
-                }
-                $supportOperator = SupportOperator::model()->findByAttributes(array('user_id' => $this->_id));
-                if ($supportOperator) {
-                    $this->setState('supportOperatorId', $supportOperator->id);
-                    $this->setState('agentId', adminAgentId());
-                    $this->setState('role',$supportOperator->role);
                     return true;
                 }
             }
