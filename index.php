a24b2a7b6ea0ab7f7c180677389edfb5a1217feb
Pavel Guscha <pavimus@gmail.com>
2013-08-07 10:08:12 +0300

    revert index.php

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 
 // this is entry script for web servers
 
-//error_reporting(E_ALL & ~E_NOTICE);
-//ini_set('display_errors','on');
-
-error_reporting(0);
-
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors','on');
 
 define('YII_TRACE_LEVEL', 3);
 
@@ -14,15 +11,53 @@ if (isset($_SERVER['HTTP_HOST']))
     $_SERVER['SERVER_NAME']=$_SERVER['HTTP_HOST'];
 
 switch ($_SERVER['SERVER_NAME']) {
+    case 'nomtel.lan':
+        define('LANGUAGE', 'ru');
+        define('TIMEZONE', 'Europe/Minsk');
+        define('YII_DEBUG', true);
+        $config = 'server_web_development.php';
+        break;
 
-        
-    
-    default:
-      define('LANGUAGE', 'ru');
-      
+    case 'po.nomtel.lan':
+        define('LANGUAGE', 'ru');
+        define('TIMEZONE', 'Europe/Minsk');
+        define('YII_DEBUG', true);
+        $config = 'server_web_po_development.php';
+        break;
+
+    case 'nomtel.asiteplace.ru':
+    case 'nomtel.asiteplace.ru:81':
+        define('LANGUAGE', 'ru');
+        define('TIMEZONE', 'Asia/Yeakaterinburg');
         define('YII_DEBUG', false);
-        $config = 'server_web_asite2_production.php';
-       break;
+        $config = 'server_web_asite_production.php';
+        break;
+
+    case 'www.500099.ru':
+    case '500099.ru':
+        define('LANGUAGE', 'ru');
+        define('TIMEZONE', 'Asia/Yeakaterinburg');
+        define('YII_DEBUG', false);
+        $config = 'server_web_po_asite_production.php';
+        break;
+
+    case 'lkdev.asiteplace.ru':
+        define('LANGUAGE', 'ru');
+        define('TIMEZONE', 'Asia/Yeakaterinburg');
+        define('YII_DEBUG', false);
+        $config = 'server_web_po_asite_development.php';
+        break;
+
+    case 'dev.asiteplace.ru':
+        define('LANGUAGE', 'ru');
+        define('TIMEZONE', 'Asia/Yeakaterinburg');
+        define('YII_DEBUG', true);
+        $config = 'server_web_asite_development.php';
+        break;
+
+    default:
+        die('unknown domain name');
+        break;
 }
 
 // include main yii file
